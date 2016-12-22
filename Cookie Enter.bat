28b37402f67b8b2cb7a9c11208d6f09d68971306
Nitrate1 <templex.mc@gmail.com>
2016-12-22 08:55:53 -0500

    Create Cookie Enter.bat

---
+++ Cookie Enter.bat
@@ -0,0 +1,20 @@
+@echo off
+set ver=0.0.0.1
+title Cookie Enter [v%ver%]
+color 0c
+
+:login
+::TODO
+if exist %%a (
+) else (
+set cookies=0
+set bakers=0
+::set TODO
+)
+
+:save
+cls
+(echo --- Save File ---) >> save.sv
+(echo cookies=%cookies%) >> save.sv
+(echo fingers=%fingers%) >> save.sv
+(echo --- File Ends ---) >> save.sv
